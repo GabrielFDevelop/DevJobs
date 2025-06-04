8de9d1737606c6cc5e3791f26badfb4c15037312
Gabriel Freitas <gabriel.freitas@bit.com.br>
2025-06-04 14:45:41 -0300

    Implementação e início de ajuste de rotas.

---
+++ devJobs/src/app/login/page.tsx
@@ -0,0 +1,5 @@
+export default function LoginPage() {
+    return(
+        <div>Página de Login</div>
+    )
+}
---
+++ devJobs/src/components/header/index.tsx
@@ -0,0 +1,32 @@
+import Link from 'next/link'
+
+export function Header(){
+    return (
+        <header className="flex px-2 py-5 bg-gray-700 text-white">
+            <div className='flex items-center justify-between w-full'>
+                <div>
+                    <h2 className='pl-10'>DevJobs</h2>
+                </div>
+                <nav>
+                    <ul className="flex items-center justify-center gap-4 pr-10">
+                        <li>
+                            <Link href="/">
+                                Home
+                            </Link>
+                        </li>    
+                        <li>
+                            <Link href="/vagas">
+                                Vagas
+                            </Link>
+                        </li>
+                        <li >
+                            <Link href="/perfil">
+                                Perfil
+                            </Link>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+    )
+}
