43550c3b09baf8581a26c709c006f01d6516de71
Gabriel Freitas <gabriel.assisreitas@outlook.com>
2025-06-04 22:22:58 -0300

    Implementação de metadatas.

--- devJobs/src/app/layout.tsx
+++ devJobs/src/app/layout.tsx
@@ -1,6 +1,26 @@
 import "./globals.css";
 
 import { Header } from '../components/header';
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'DevJobs',
+  description: 'Página de aplicação de vagas de emprego.',
+  openGraph: {
+    title: 'DevJobs',
+    description: 'Venha encontrar seu emprego!',
+    images: []
+  },
+  robots: {
+    index: true,
+    follow: true,
+    nocache: true,
+    googleBot: {
+      index: true,
+      follow: true,
+    }
+  },
+}
 
 export default function RootLayout({
   children,
