0f51c26fbc47c4215b615edc1557bd205a6ddc99
Gabriel Freitas <gabriel.freitas@bit.com.br>
2025-06-05 11:50:42 -0300

    Criando chamada da api pra apresentação das vagas.

--- devJobs/src/app/layout.tsx
+++ devJobs/src/app/layout.tsx
@@ -3,14 +3,17 @@ import "./globals.css";
 import { Header } from '../components/header';
 import { Metadata } from 'next'
 
+// Metadados da aplicação web
 export const metadata: Metadata = {
   title: 'DevJobs',
   description: 'Página de aplicação de vagas de emprego.',
+// openGraph é usado para definir metadados para compartilhamento em redes sociais, é o que vai aparecer para as pessoas ao compartilhar o link da aplicação
   openGraph: {
     title: 'DevJobs',
     description: 'Venha encontrar seu emprego!',
     images: []
   },
+// robots controlam a quantidade de conteúdo que o Google extrai automaticamente de páginas da web para exibição como resultados de pesquisa.
   robots: {
     index: true,
     follow: true,
