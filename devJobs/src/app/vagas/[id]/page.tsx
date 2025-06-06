14913a86fd319b07a63027bae3238f59b38abc00
Gabriel Freitas <gabriel.freitas@bit.com.br>
2025-06-06 14:24:21 -0300

    Leve conhecimento sobre client e server components.

--- devJobs/src/app/vagas/[id]/page.tsx
+++ devJobs/src/app/vagas/[id]/page.tsx
@@ -1,4 +1,5 @@
-'use client'
+'use client'; //para definir que este é um componente do lado do cliente
+'use server'; //para definir que este é um componente do lado do servidor
 
 // Client components não podem ser assíncronos e devem ser usados para interações do usuário, como eventos de clique, formulários, etc.
 // Server components são usados para renderizar conteúdo estático e podem ser assíncronos, permitindo chamadas de API diretamente no componente. Mas não podem ser reativos.
