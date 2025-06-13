c2049b4b4524165a86914546b913a41b41d3f09a
Gabriel Freitas <gabriel.freitas@bit.com.br>
2025-06-13 13:50:14 -0300

    Alterações nas identações.

--- devJobs/src/app/login/page.tsx
+++ devJobs/src/app/login/page.tsx
@@ -1,31 +1,31 @@
 import style from './login.module.css';
 
 export default function LoginPage() {
-    return(
-        <div className={style.bodyLogin}>
-            <div className={style.boxLogin}>
-            <h1 className={style.titleLogin}>Seja bem vindo!</h1>
-                <div className={style.boxListLogin}>
-                    <ul className={style.listLogin}>
-                        <li>
-                            <p>Email</p>
-                            <input type='text' name='email' className={style.inputEmail} placeholder='seuemail@exxxemplo.com' />
-                        </li>
-                        <li>
-                            <p>Senha</p>
-                            <input type='password' name='senha' className={style.inputSenha} placeholder='Digite sua senha' />    
-                        </li>
-                        <li>
-                            <button type='submit' name='buttonLogin' className={style.buttonEntrar}>Entrar</button>
-                        </li>
-                    </ul>
-                    <a href='/trocaSenha'>Esqueceu a senha?</a>
-                </div>
-                <div className={style.boxRedirectCadastro}>
-                    <h3 className={style.textRedirectCadastro}>Ainda não tem uma conta?</h3>
-                    <a className={style.buttonRedirectCadastro} href='/cadastro'>Cadastre-se</a>
-                </div>
-            </div>
-        </div>
-    )
+	return(
+		<div className={style.bodyLogin}>
+			<div className={style.boxLogin}>
+			<h1 className={style.titleLogin}>Seja bem vindo!</h1>
+				<div className={style.boxListLogin}>
+					<ul className={style.listLogin}>
+						<li>
+							<p>Email</p>
+							<input type='text' name='email' className={style.inputEmail} placeholder='seuemail@exxxemplo.com' />	
+						</li>
+						<li>
+							<p>Senha</p>
+							<input type='password' name='senha' className={style.inputSenha} placeholder='Digite sua senha' />    
+						</li>
+						<li>
+							<button type='submit' name='buttonLogin' className={style.buttonEntrar}>Entrar</button>
+						</li>
+					</ul>
+					<a href='/trocaSenha'>Esqueceu a senha?</a>
+				</div>
+				<div className={style.boxRedirectCadastro}>
+					<h3 className={style.textRedirectCadastro}>Ainda não tem uma conta?</h3>
+					<a className={style.buttonRedirectCadastro} href='/cadastro'>Cadastre-se</a>
+				</div>
+			</div>
+		</div>
+	)
 }
--- devJobs/src/app/not-found.tsx
+++ devJobs/src/app/not-found.tsx
@@ -1,14 +1,14 @@
 import Link from "next/link"
 
 export default function NotFound(){
-    return(
-        <div className="flex flex-col items-center justify-center">
-            <h1 className="text-center font-bold mt-20 text-3xl">404 - Página não encontrada!</h1>
-            <p className="mt-3">A página que você está procurando não existe.</p>
+	return(
+		<div className="flex flex-col items-center justify-center">
+			<h1 className="text-center font-bold mt-20 text-3xl">404 - Página não encontrada!</h1>
+			<p className="mt-3">A página que você está procurando não existe.</p>
 
-            <Link href="/">
-                Voltar para Home
-            </Link>
-        </div>
-    )
+			<Link href="/">
+				Voltar para Home
+			</Link>
+		</div>
+	)
 }
