dd841f86b007f9af4441214b34183da05b714b1c
Gabriel Freitas <gabriel.freitas@bit.com.br>
2025-06-16 13:45:10 -0300

    Estilização da tela de Login realizada.

--- devJobs/src/app/login/page.tsx
+++ devJobs/src/app/login/page.tsx
@@ -1,29 +1,37 @@
 import style from './login.module.css';
+import ImgBoxLogin from '@/components/imgBoxLogin';
 
 export default function LoginPage() {
 	return(
 		<div className={style.bodyLogin}>
 			<div className={style.boxLogin}>
-			<h1 className={style.titleLogin}>Seja bem vindo!</h1>
-				<div className={style.boxListLogin}>
-					<ul className={style.listLogin}>
-						<li>
-							<p>Email</p>
-							<input type='text' name='email' className={style.inputEmail} placeholder='seuemail@exxxemplo.com' />	
-						</li>
-						<li>
-							<p>Senha</p>
-							<input type='password' name='senha' className={style.inputSenha} placeholder='Digite sua senha' />    
-						</li>
-						<li>
-							<button type='submit' name='buttonLogin' className={style.buttonEntrar}>Entrar</button>
-						</li>
-					</ul>
-					<a href='/trocaSenha'>Esqueceu a senha?</a>
+				<div className={style.topBoxLogin}>
+					<h1 className={style.titleLogin}>Seja bem vindo!</h1>
 				</div>
-				<div className={style.boxRedirectCadastro}>
-					<h3 className={style.textRedirectCadastro}>Ainda não tem uma conta?</h3>
-					<a className={style.buttonRedirectCadastro} href='/cadastro'>Cadastre-se</a>
+				<div className={style.midBoxLogin}>
+					<div className={style.boxListLogin}>
+						<ul className={style.listLogin}>
+							<li>
+								<p>Email</p>
+								<input type='text' name='email' className={style.inputEmail} placeholder='seuemail@exxxemplo.com' />	
+							</li>
+							<li>
+								<p>Senha</p>
+								<input type='password' name='senha' className={style.inputSenha} placeholder='Digite sua senha' />    
+							</li>
+							<li>
+								<button type='submit' name='buttonLogin' className={style.buttonEntrar}>Entrar</button>
+							</li>
+						</ul>
+						<a href='/trocaSenha'>Esqueceu a senha?</a>
+						<div className={style.boxRedirectCadastro}>
+							<h3 className={style.textRedirectCadastro}>Ainda não tem uma conta?</h3>
+							<a className={style.buttonRedirectCadastro} href='/cadastro'>Cadastre-se</a>
+						</div>
+					</div>
+					<div className={style.imgBoxLogin}>
+						<ImgBoxLogin />
+					</div>
 				</div>
 			</div>
 		</div>
