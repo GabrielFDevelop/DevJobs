058ee8495f36f7156b802986cedee8962da3781a
Gabriel Freitas <gabriel.freitas@bit.com.br>
2025-06-27 16:48:19 -0300

    Implementação inicial da homepage

--- devJobs/src/app/page.tsx
+++ devJobs/src/app/page.tsx
@@ -1,7 +1,29 @@
 export default function Home(){
   return(
     <div>
-      <h1>Página Home</h1>
+      <div>
+        <h1>Sobre o Projeto</h1>
+      </div>
+      <div>
+        <h2>Qual o propósito do projeto?</h2>
+        <p>Texto sobre propósito.</p>
+      </div>
+      <div>
+        <h2>Quais tecnologias foram utilizadas?</h2>
+        <p>Texto sobre tecnologias.</p>
+      </div>
+      <div>
+        <h2>Como encontrar as vagas para você?</h2>
+        <p>Texto sobre como encontrar vagas.</p>
+      </div>
+      <div>
+        <h2>Como cadastrar uma vaga?</h2>
+        <p>Texto sobre como cadastrar vagas.</p>
+      </div>
+      <div>
+        <h2>Quem desenvolveu o projeto?</h2>
+        <p>Texto sobre os desenvolvedores.</p>
+      </div>
     </div>
   )
 }
