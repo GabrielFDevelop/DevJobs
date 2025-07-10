2fee4c7a979f0b2fff91b65c4e723fb9e6ca1043
Gabriel Freitas <gabriel.freitas@bit.com.br>
2025-07-10 14:37:31 -0300

    Implementações no frontend com relação à deletar o usuário na página de segurança em perfil.

--- devJobs/src/components/perfil/perfilSeguranca/index.tsx
+++ devJobs/src/components/perfil/perfilSeguranca/index.tsx
@@ -1,7 +1,40 @@
+'use client';
+
 import style from './perfilSeguranca.module.css';
 import React from 'react';
 
 export default function PerfilSeguranca() {
+  
+  const handleDelete = async (e: React.FormEvent) => {
+    const confirmDelete = window.confirm('Você tem certeza que deseja excluir seu perfil? Esta ação não pode ser desfeita.');
+    if (!confirmDelete) {
+      return;
+    }
+
+    try {
+      const userId = localStorage.getItem('userId');
+      if (!userId) {
+        alert('Usuário não encontrado.');
+        return;
+      }
+
+      const response = await fetch(`http://localhost:3001/users/${userId}`, {
+        method: 'DELETE',
+      });
+
+      if (!response.ok) {
+        throw new Error('Erro ao excluir perfil.');
+      }
+
+      localStorage.removeItem('userId');
+      alert('Perfil excluido com sucesso!');
+      window.location.href = '/login';
+    } catch (error) {
+      console.error('Erro ao excluir perfil:', error);
+      alert('Erro ao excluir perfil.');
+    }
+  };
+
   return (
     <main className={style.contentSegurancaPerfil}>
       <section className={style.sectionTitle}>
@@ -12,7 +45,13 @@ export default function PerfilSeguranca() {
           <li>
             <label  className={style.spanItemListConfigPerfil}>
               <p>Deseja excluir seu perfil?</p>
-              <button className={style.buttonDeletePerfil} type="submit">Excluir Perfil</button>
+              <button 
+                onClick={handleDelete} 
+                className={style.buttonDeletePerfil} 
+                type="submit"
+              >
+                Excluir Perfil
+              </button>
             </label>
           </li>
         </ul>
