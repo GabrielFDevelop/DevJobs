9119ec7d3243472a5dfaa59205df71621dadf28e
Gabriel Freitas <gabriel.freitas@bit.com.br>
2025-07-23 15:03:22 -0300

    Nova estilização feita na página Not-found

--- devJobs/src/app/not-found.tsx
+++ devJobs/src/app/not-found.tsx
@@ -7,7 +7,7 @@ import ImgHeaderHome from '@/components/header/headerIcons/imgHeaderHome';
 export default function NotFound(){
 	return(
 		<div className="flex flex-col items-center pt-20 pb-109 justify-center">
-			<div className='flex flex-col items-center justify-center p-20 bg-white rounded-4xl shadow-lg '>
+			<div className='flex flex-col items-center justify-center text-white p-20 rounded-4xl bg-gray-800 shadow-lg border-2 border-blue-950 hover:outline-blue-800 hover:outline-2 hover:scale-x-110 hover:scale-y-105 transition-all duration-1800'>
 				<span className="flex items-center gap-3 mt-20">
 					<ImgCrossCloud width={180} />
 					<ImgArrowRight width={100} />
@@ -17,7 +17,7 @@ export default function NotFound(){
 				<p className="mt-3 text-3xl">A página que você está procurando não existe.</p>
 
 				<Link href="/" className='text-2xl'>
-					<span className="flex items-center gap-3 mt-10 text-blue-500 hover:text-blue-700 transform-3d hover:scale-120 transition-all ease-in-out duration-400">
+					<span className="flex items-center gap-3 mt-10 text-blue-500 hover:text-blue-700 transform-3d hover:scale-120 transition-all ease-in-out duration-00">
 						Voltar para Home
 						<ImgHeaderHome width={40} />
 					</span>
