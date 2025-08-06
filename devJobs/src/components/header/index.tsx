a324b39f3480f8b9f9e67b4c15930114c16f4fcc
Gabriel Freitas <gabriel.freitas@bit.com.br>
2025-08-06 12:06:13 -0300

    Alteração na estilização do header

--- devJobs/src/components/header/index.tsx
+++ devJobs/src/components/header/index.tsx
@@ -1,39 +1,36 @@
 import Link from 'next/link';
-import ImgPerfil from './headerIcons/imgIconPerfil/iconPerfil';
-import ImgBoxCadastro from './headerIcons/imgBoxCadastro';
-import ImgBoxLogin from './headerIcons/imgBoxLogin/imgBoxLogin';
 import ImgVagas from './headerIcons/imgVagas';
 import ImgHeaderHome from './headerIcons/imgHeaderHome';
 import ImgDevJ from './headerIcons/imgIconDevJ/imgIconDevJ';
 
 export function Header(){
 	return (
-		<header className="flex px-2 py-4 bg-gradient-to-br from-gray-800 to-blue-900 to-gradient text-white">
-			<div className='flex items-center justify-between w-full'>
-				<div  className='pl-10  hover:scale-x-110 transition-all duration-600'>
+		<header className="flex justify-center align-center px-2 border-b-2 border-blue-800 border-gradient bg-gradient-to-br from-gray-800 to-blue-900 to-gradient text-white">
+			<div className='flex flex-row justify-center items-center w-full gap-50'>
+				<div  className='pl-10 flex items-center hover:scale-x-110 transition-all duration-600'>
 					<Link href={"/"} >
 						<span	className="flex items-center gap-2">
-							<ImgDevJ width={60} />
-							<h2 className='text-5xl font-extrabold text-justify'>DevJobs</h2>
+							<ImgDevJ width={40} />
+							<h2 className='text-2xl font-extrabold text-justify'>DevJobs</h2>
 						</span>
 					</Link>
 				</div>
-				<div className='flex items-center justify-center pr-30'>
+				<div className='flex items-center justify-center'>
 					<nav>
-						<ul className="flex items-center justify-center gap-8">
-							<li className='flex items-center gap-1 '>
-								<Link href={"/"} className='text-white mr-3 text-2xl hover:text-gray-300 transform-3d hover:scale-105 transition-all duration-400 ease-in-out'>
+						<ul className="flex items-center justify-center">
+							<li className='flex items-center gap-1 py-2 border-b-2 border-transparent hover:border-blue-500 transition-all duration-1200'>
+								<Link href={"/"} className='text-white px-3 hover:text-gray-300 transform-3d hover:scale-105 transition-all duration-400 ease-in-out'>
 									<span className="flex items-center gap-2">
 										Home
-										<ImgHeaderHome width={60} />
+										<ImgHeaderHome width={30} />
 									</span>
 								</Link>
 							</li>    
-							<li className='flex items-center gap-1 '>
-								<Link href={"/vagas"} className='text-white mr-5 text-2xl hover:text-gray-300 transform-3d hover:scale-105 transition-all duration-400 ease-in-out'>
+							<li className='flex items-center gap-1 py-2 border-b-2 border-transparent hover:border-blue-500 transition-all duration-1200'>
+								<Link href={"/vagas"} className='text-white px-3 hover:text-gray-300 transform-3d hover:scale-105 transition-all duration-400 ease-in-out'>
 									<span className="flex items-center gap-2">
 										Vagas
-										<ImgVagas width={60} />
+										<ImgVagas width={30} />
 									</span>
 								</Link>
 							</li>
